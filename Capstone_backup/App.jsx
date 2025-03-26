67b290ab4b84299cd136b645152ba5ff04bc9ca7
lchang4351 <lchang4351@gmail.com>
2025-03-26 16:59:42 +0800

    Backup (26-Mar)

---
+++ Capstone_backup/App.jsx
@@ -0,0 +1,20 @@
+import { useEffect, useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+
+import background from "./wallpaper.jpg"
+import './form.css'
+import Form from './form.jsx'
+
+
+function App() {
+
+  return(
+    <>
+      <h1>Finance Dashboard</h1>
+      <Form />
+    </>
+  )
+}
+
+export default App
